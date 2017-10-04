2007f08abb7bd2d4aacffe05946cd31a8c74d7de
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-10-03 21:14:12 -0500

    added new routes to edit API

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,41 @@
 class EmployeesController < ApplicationController
 
+  def index
+    @employees = Unirest.get('localhost:3000/api/v2/employees.json').body
+  end
+
+  def new
+    
+  end
+
+  def create
+    @employee = Unirest.post(
+                            "localhost:3000/api/v2/employees",
+                            headers:{ "Accept" => "application/json" }, 
+                            parameters:{
+                                        first_name: params[:first_name],
+                                        last_name: params[:last_name],
+                                        email: params[:email]
+                                        }
+                            ).body
+
+    redirect_to "/employees/#{ @employee['id'] }"
+  end
+
   def show
     @employee = Unirest.get("localhost:3000/api/v2/employees/#{params[:id] }.json").body
+  end
+
+  def update
+    @employees = Unirest.patch(
+                              "localhost:3000/api/v2/employees/#{params[:id]}",
+                              headers: { "Accept" => "application/json" },
+                              paramaters: {
+                                          first_name: params[:first_name],
+                                          last_name: params[:last_name],
+                                          email: params[:email]
+                                          }
+                              ).body
   end
 
 end
